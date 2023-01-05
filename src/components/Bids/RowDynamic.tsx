90dfa74faa86f5fb37d296aad72d897a8d2ed64e
Andrey Babkin <babkin.andrey@gmail.com>
2023-01-05 18:50:16 +0300

    websockets setup

--- src/components/Bids/RowDynamic.tsx
+++ src/components/Bids/RowDynamic.tsx
@@ -1,33 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
+import { io, Socket } from 'socket.io-client';
 import { TableRow, TableCell, Button } from '@mui/material';
 import convertSecondsFromString from '../../helpers/convertSecondsToString';
 
-const timerValue = 120;
+const timerValue = 30;
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  timerReset: (counter: number, currentUser: number) => void;
+	currentTimer: (counter: number, currentUser: number) => void;
+}
+
+interface ClientToServerEvents {
+	timerSkip: () => void;
+}
 
 const RowDynamic = ({ participantIdList }: { participantIdList: string[] }) => {
   const [activeParticipant, setActiveParticipant] = useState(0);
-	const [timeLeft, setTimeLeft] = useState(timerValue);
-	const [currentTurn, setCurrentTurn] = useState(false);
-  // useEffect(() => {
- 
+  const [timeLeft, setTimeLeft] = useState(timerValue);
+  const [currentTurn, setCurrentTurn] = useState(false);
+  
+	const cachedSocket: Socket<ServerToClientEvents, ClientToServerEvents> = useMemo(()=> io(
+    'http://localhost:8080'
+  ), [])
+  cachedSocket.on('currentTimer', (counter, currentUser) => {
+    setTimeLeft(counter);
+		setActiveParticipant(currentUser)
+  });
+	cachedSocket.on('timerReset', (counter, currentUser) => {
+		setTimeLeft(counter);
+		setActiveParticipant(currentUser);
+	})
 
-  //   const timerId = setInterval(() => {
-			
-	// 		if (timeLeft === 0) {
-	// 			if (activeParticipant < participantIdList.length-1) {
+	const onButtonClickHandler = () => {
+		console.log('fired!');
+		cachedSocket.emit('timerSkip');
+	}
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      if (timeLeft === 0) {
+        
+        setTimeLeft(0);
+      } else {
+        setTimeLeft((previous) => previous - 1);
+      }
+    }, 1000);
 
-	// 				setActiveParticipant((previous) => previous + 1);
-	// 			} else {
-	// 				setActiveParticipant(0);
-	// 			}
-	// 			setTimeLeft(timerValue)
-	// 		} else {
-	// 			setTimeLeft((previous) => previous - 1);
-	// 		}
-  //   }, 1000);
-
-	// 	return () => clearInterval(timerId);
-  // }, [timeLeft, activeParticipant, participantIdList]);
+    return () => clearInterval(timerId);
+  }, [timeLeft, activeParticipant, participantIdList]);
   return (
     <TableRow
       sx={{
@@ -39,24 +59,31 @@ const RowDynamic = ({ participantIdList }: { participantIdList: string[] }) => {
       <TableCell sx={{ height: '80px' }}></TableCell>
       {participantIdList.map((el) => (
         <TableCell key={el} align="center">
-          {participantIdList[activeParticipant] === el && ( currentTurn ? <Button
-              variant="contained"
-							color="success"
-              disableElevation
-              sx={{ width: '100%', lineHeight: '2.75'   }}
-            >
-              Ваш ход!
-            </Button> : 
-            <Button
-              variant="contained"
-              disableElevation
-							disabled
-              sx={{ width: '100%', lineHeight: '2.75', backgroundColor: '#EDBFBF !important', color: 'red !important'   }}
-            >
-              {convertSecondsFromString(timeLeft)}
-            </Button> 
-            
-          )}
+          {participantIdList[activeParticipant] === el &&
+            (currentTurn ? (
+              <Button
+                variant="contained"
+                color="success"
+                disableElevation
+                sx={{ width: '100%', lineHeight: '2.75' }}
+              >
+                Ваш ход!
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                disableElevation              
+                sx={{
+                  width: '100%',
+                  lineHeight: '2.75',
+                  backgroundColor: '#EDBFBF !important',
+                  color: 'red !important',
+                }}
+								onClick={onButtonClickHandler}
+              >
+                {convertSecondsFromString(timeLeft)}
+              </Button>
+            ))}
         </TableCell>
       ))}
     </TableRow>
